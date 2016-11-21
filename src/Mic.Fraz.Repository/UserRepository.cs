dcce5ac1aa1453a1da7a121637bcacde85a15e21
Michael Demarco <miczdem@gmail.com>
2016-11-22 08:37:48 +1100

    removed compiler warning for async missing method

--- src/Mic.Fraz.Repository/UserRepository.cs
+++ src/Mic.Fraz.Repository/UserRepository.cs
@@ -11,13 +11,18 @@ namespace Mic.Fraz.Repository
         {
         }
 
+        #pragma warning disable 1998
         public async Task<User> GetAsync(int id)
-        {            
+        {     
+            // TODO: replace with an async database operation  
             return new User()
             {
                 Id = id,
                 Name = $"Name-{id}-{Guid.NewGuid()}"
             };
         }
+        #pragma warning restore 1998
+        
     }
 }
+
